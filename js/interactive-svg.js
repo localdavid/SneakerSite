4f7f84103e50f6bd2f7723c6434918e7f91f7d5b
David Kroll <david.s.kroll@gmail.com>
2014-03-10 21:47:44 -0500

    Added stage to css file

--- js/interactive-svg.js
+++ js/interactive-svg.js
@@ -1,6 +1,6 @@
 $(function(){
 
-	// jQuery.load loads the SVG file into our #stage div and give it the class svgLoaded which we’ll later use to trigger our intro animation
+	// jQuery.load loads the SVG file into our #stage div and give it the class svgLoaded which we’ll later use to trigger our intro animation. Important: We load the SVG using JavaScript to gain access to its DOM. Chrome (and maybe other browsers) will not let you do this locally; it only works when run from the HTTP protocol for security reasons. So if you are having issues getting the SVG to load, be sure you are testing from a web server or running on localhost
 	
 	$("#stage").load('stripe1.svg',function(response){
 
